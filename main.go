974c78b13eae4f81a571eddb08733f40ac0cfa40
wangyiwei <wangyiwei@seu.edu.cn>
2021-01-06 21:42:23 +0800

    1

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+//index函数用来返回主页面
+func index(w http.ResponseWriter, r *http.Request) {
+	buf, err := ioutil.ReadFile("views/index.html")
+	if err != nil {
+		panic(err)
+	}
+	w.Write(buf)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	http.Handle("/pages/", http.StripPrefix("/pages/", http.FileServer(http.Dir("views/pages/"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("views/static/"))))
+	http.Handle("/notes/", http.StripPrefix("/notes/", http.FileServer(http.Dir("views/notes/"))))
+	http.HandleFunc("/index", index)
+	fmt.Println("Ip:0.0.0.0\nPort:8080")
+	http.ListenAndServe(":8080", nil)
 }
